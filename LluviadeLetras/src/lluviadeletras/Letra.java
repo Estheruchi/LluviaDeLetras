92a322c401cfde48bd6e3dc0de9e375ff9da4288
Estheruchi <esthergs9@hotmail.com>
2018-02-01 09:00:02 +0100

    Modificacion

--- LluviadeLetras/src/lluviadeletras/Letra.java
+++ LluviadeLetras/src/lluviadeletras/Letra.java
@@ -16,42 +16,36 @@ import javax.swing.JLabel;
 public class Letra extends JButton {
 
     /*VARIABLES COMUNES*/
-    private static final String[] LETRAS = {"a", "b", "c", "d", "f", "g",
-        "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-        "u", "v", "w", "x", "y", "z"};
     private static final int ALTO = 50;
     private static final int ANCHO = 50;
-
-    private static String[] estado = new String[10];
-    private static int c = 0;
 
     /*VARIABLES DE CADA PANEL*/
     private Modelo modelo;
     private Color color;
     private String letra;
+    private boolean estado;
+
+    /*TRUE: CAYENDO FALSE: OCULTO*/
     private int posX;
     private int posY = - 50;
 
 
     /*CONSTRUCTOR*/
-    public Letra(Modelo modelo) {
+    public Letra(Modelo modelo, String letra) {
         this.modelo = modelo;
-        cargarEstado();
+        this.letra = letra;
+        this.estado = false;
         generarColor();
-        generarLetra();
         generarPosicion();
         dibujarPanel();
     }
 
-    public void cargarEstado() {
-        if (c == 0) {
-            for (int i = 0; i < 10; i++) {
-                estado[i] = "";
-            }
-        } else if (c == 10) {
-            c = 0;
-            cargarEstado();
-        }
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public boolean isEstado() {
+        return estado;
     }
 
     public void generarColor() {
@@ -60,34 +54,6 @@ public class Letra extends JButton {
         int b = (int) Math.round(Math.random() * 255);
         color = new Color(r, g, b);
         this.setBackground(color);
-    }
-
-    public void reiniciarLetra(int i) {
-        c = i;
-        cargarEstado();
-        generarLetra();
-    }
-
-    public void generarLetra() {
-        int n = (int) Math.floor(Math.random() * 25);
-        if (comprobar(n)) {
-            estado[c] = LETRAS[n];
-            this.setText(LETRAS[n]);
-            c++;
-        } else {
-            generarLetra();
-        }
-    }
-
-    public boolean comprobar(int n) {
-        if (c != 0) {
-            for (int i = 0; i <= c; i++) {
-                if (estado[i].equals(LETRAS[n])) {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 
     public void generarPosicion() {
--- LluviadeLetras/src/lluviadeletras/Modelo.java
+++ LluviadeLetras/src/lluviadeletras/Modelo.java
@@ -5,19 +5,35 @@
  */
 package lluviadeletras;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.Timer;
+
 /**
  *
  * @author Estheruchi
  */
 public class Modelo {
 
+    private int NUM_LETRAS = 25;
+    private final String[] ABC = {"a", "b", "c", "d", "f", "g",
+        "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+        "u", "v", "w", "x", "y", "z"};
+
+    private int tiempoCaida = 10;
+    private int tiempoCreacion = 1000;
+
     private Controlador control;
     private Bandeja bandeja;
+    private ArrayList<Letra> letras;
 
     public Modelo(Controlador control) {
         this.control = control;
         this.bandeja = new Bandeja(this);
         manejarBandeja();
+        crearLetras();
+        timerCrear();
     }
 
     public void manejarBandeja() {
@@ -40,5 +56,43 @@ public class Modelo {
 
     public void moverIzqda() {
         bandeja.mover(1);
+    }
+
+    public void crearLetras() {
+        letras = new ArrayList();
+        for (int i = 0; i < NUM_LETRAS; i++) {
+            letras.add(new Letra(this, ABC[i]));
+        }
+    }
+
+    public void letraAleatoria() {
+        int indice = (int) Math.floor(Math.random() * 25);
+        if (comprobar()) {
+            System.out.println("CAE " + indice);
+            System.out.println(letras.get(indice));
+            letras.get(indice).setEstado(true);
+            letras.get(indice).setVisible(true);
+        } else {
+            letraAleatoria();
+        }
+    }
+
+    public boolean comprobar() {
+        for (int i = 0; i < letras.size(); i++) {
+            if (!letras.get(i).isEstado()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void timerCrear() {
+        Timer temporizador = new Timer(tiempoCreacion, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                letraAleatoria();
+            }
+        });
+        temporizador.start();
     }
 }
