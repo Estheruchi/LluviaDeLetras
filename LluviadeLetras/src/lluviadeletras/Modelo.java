332aba48bdb148df74adfb577665996338e67490
Estheruchi <esthergs9@hotmail.com>
2018-02-07 13:25:08 +0100

    Comentarios y cambios

--- LluviadeLetras/src/lluviadeletras/Modelo.java
+++ LluviadeLetras/src/lluviadeletras/Modelo.java
@@ -26,17 +26,20 @@ public class Modelo {
 
     private Controlador control;
     private Bandeja bandeja;
+
     private ArrayList<Letra> letras;
     private ArrayList<Timer> tiemposCaida;
+
     private Timer temporizadorCrear;
     private int idTimer;
-    //private Timer temporizadorCaer;
+    private int puntuacion;
 
     public Modelo(Controlador control) {
         this.control = control;
         this.bandeja = new Bandeja(this);
         tiemposCaida = new ArrayList();
         this.idTimer = 0;
+        this.puntuacion = 0;
 
         manejarBandeja();
         crearLetras();
@@ -44,28 +47,47 @@ public class Modelo {
 
     }
 
+    /**
+     * Dibujamos la bandeja.
+     */
     public void manejarBandeja() {
         control.dibujarBandeja(bandeja);
     }
 
+    /**
+     * Obtenemos el ancho de la ventana
+     */
     public int anchoVentana() {
         int x = control.anchoVentana();
         return x;
     }
 
+    /**
+     * Obtenemos el alto de la ventana
+     */
     public int altoVentana() {
         int y = control.altoVentana();
         return y;
     }
 
+    /**
+     * Mueve la bandeja a la Derecha
+     */
     public void moverDcha() {
         bandeja.mover(0);
     }
 
+    /**
+     * Mueve la bandeja a la Izquierda
+     */
     public void moverIzqda() {
         bandeja.mover(1);
     }
 
+    /**
+     * Llenamos el array de letras con tantas letras como se indique en
+     * NUM_LETRAS
+     */
     public void crearLetras() {
         letras = new ArrayList();
         for (int i = 0; i < NUM_LETRAS; i++) {
@@ -73,43 +95,56 @@ public class Modelo {
         }
     }
 
+    /**
+     * Genera un numero aleatorio que funcionara como indice. Si la letra
+     * correspondiente a dicho indice no ha caido cae. Si, sin embargo está
+     * cayendo, busca otra.
+     *
+     * Si encuentra que no esta cayendo, si es la primera vez crea un timer si
+     * ya habia caido, vuelve a iniciar su timer.
+     *
+     */
     public void letraAleatoria() {
         int indice = (int) Math.floor(Math.random() * 24);
+        Letra generada = letras.get(indice);
 
-        if (!letras.get(indice).isEstado()) {
+        if (!generada.isEstado()) {
 
-            System.out.println("CAE " + indice);
-            System.out.println(letras.get(indice));
+            generada.setEstado(true);
+            generada.setPosY(-50);
 
-            letras.get(indice).setEstado(true);
-            letras.get(indice).setPosY(-50);
+            control.dibujarLetra(generada);
+            generada.setText(ABC[indice]);
 
-            control.dibujarLetra(letras.get(indice));
-            letras.get(indice).setText(ABC[indice]);
+            if (generada.getIdTimer() != -1) {
+                generada.setVisible(true);
+                tiemposCaida.get((generada.getIdTimer())).start();
 
-            if (letras.get(indice).getIdTimer() != -1) {
-                System.out.println("PRIMERA CAIDA");
-                letras.get(indice).setVisible(true);
-                tiemposCaida.get((letras.get(indice).getIdTimer())).start();
             } else {
-                timerCaer(letras.get(indice));
-                System.out.println("VUELVE A CAER");
-                letras.get(indice).setVisible(true);
+                timerCaer(generada);
+                generada.setVisible(true);
 
             }
             control.refrescar();
+
         } else {
             letraAleatoria();
         }
     }
 
+    /**
+     *
+     * @param letra
+     */
     public void buscarLetra(String letra) {
         letra = letra.toUpperCase();
         for (int i = 0; i < letras.size(); i++) {
             if (letras.get(i).getText().equals(letra)) {
+
                 letras.get(i).setVisible(false);
                 letras.get(i).setEstado(false);
                 letras.get(i).setPosY(-50);
+
                 tiemposCaida.get((letras.get(i).getIdTimer())).stop();
             }
         }
